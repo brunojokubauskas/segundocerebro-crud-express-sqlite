e2330476dbdc6d8fcc82c897c4626bb9c88e1e40
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-03-18 00:47:35 -0300

    Issue arruma na linha 36, notesController

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,52 +1,57 @@
-const knex = require('../database/knex') 
+const knex = require('../database/knex');
 
 class NotesController {
-   async create(request, response){
-    //desestruturando o conteúdo.,,l
-      const {title, description, tags, links } = request.body;
-      const {user_id} = request.params; 
+  async create(request, response) {
+    const { title, description, tags, links } = request.body;
+    const { user_id } = request.params;
 
-      const note_id = await knex("notes").insert({
-        title, 
-        description,
-        user_id
-        
-      });
-
-      const linksInsert = links.map(link => {
-        return {
-          note_id,
-          url: link
-
-
-        }
-      });
-
-      await knex("links").insert(linksInsert); 
-
-      const tagsInsert = tags.map(name => {
-        return {
-          note_id,
-          name,
-          user_id 
-        }
-      });
-
-      await knex("tags").insert(tagsInsert); 
-      response,json();
-    } 
-
-    async show(request,response){
-      const { id } = request.params;
-
-      const note = await knex("notes").where({id}).first(); 
-      const tags = await knex("tags").where({note_id: id}).orderBy("name");
-      const links = await knex("links").where({note_id: id}).orderBy("created_at");
-      return response.json({
-        ...note,
-        tags,
-        links
-      });
+    // Verifica se o usuário existe antes de inserir a nota
+    const user = await knex('users').where({ id: user_id }).first();
+    if (!user) {
+      return response.status(404).json({ error: 'Usuário não encontrado' });
     }
+
+    const [note_id] = await knex('notes').insert({
+      title,
+      description,
+      user_id
+    });
+
+    const linksInsert = links.map(link => {
+      return {
+        note_id,
+        url: link
+      };
+    });
+
+    await knex('links').insert(linksInsert);
+
+    const tagsInsert = tags.map(name => {
+      return {
+        note_id,
+        name,
+        user_id
+      };
+    });
+
+    await knex('tags').insert(tagsInsert);
+
+    response.json({ note_id });
   }
-module.exports =  NotesController;
+
+  async show(request, response) {
+    const { id } = request.params;
+
+    const note = await knex('notes').where({ id }).first();
+    const tags = await knex('tags').where({ note_id: id }).orderBy('name');
+    const links = await knex('links').where({ note_id: id }).orderBy('created_at');
+
+    response.json({
+      ...note,
+      tags,
+      links
+    });
+  }
+}
+
+module.exports = NotesController;
