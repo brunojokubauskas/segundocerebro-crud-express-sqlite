b32c80f2ab1ba6203f71c53390a44f3f5f5cba0f
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-03-18 11:20:44 -0300

    Att, notesController, assync index

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -62,12 +62,39 @@ class NotesController {
 
   async index(request, response){
     //Vai ser responsável por listar... 
-    const {user_id} = request.query;
-    const notes = await knex('notes')
-    .where({user_id})
-    .orderBy("title") 
+    const {title, user_id, tags} = request.query;
+    let notes;
+    if(tags){
+      const filterTags  = tags.split('').map(tag => tag.trim()); 
 
-    return response.json(notes);
+      notes = await knex("tags")
+      .select([
+        "notes.id",
+        "notes.title",
+        "notes.user_id"
+      ])
+      .where("notes.user", user_id )
+      .whereLike("notes.title", `%${title}%`)
+      .whereIn(("name", filterTags))
+      .innerJoin("notes", "notes.id", "tags.note_id")
+    }else{
+      notes = await knex('notes')
+     .where({user_id})
+     .whereLike("title", `%${title}%`)
+     .orderBy("title") 
+  } 
+
+  const userTags = await knex("knex").where({user_id})
+  const notesWithTags = notes.map(note => {
+    const noteTags = userTags.filter(tag => tag.note_id === note.id)
+
+    return {
+      ...note,
+         tags: noteTags
+    };
+  })
+
+    return response.json(notesWithTags);
   }
 }
 
