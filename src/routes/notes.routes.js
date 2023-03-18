244cf8adeedb43153f3cbbc19d4340d06d3680a4
Bruno Jokubauskas <brunojokubauskas@gmail.com>
2023-03-18 01:00:08 -0300

    att add

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -8,5 +8,6 @@ const notesController = new NotesController();
 
 notesRoutes.post("/:user_id", notesController.create.bind(notesController));
 notesRoutes.get("/:id", notesController.show.bind(notesController));
+notesRoutes.delete("/:id", notesController.delete);
 
 module.exports = notesRoutes;
